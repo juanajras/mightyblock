100b137cc8c7c8112bd9ed8f5316763a858ecc13
Juan Ajras <juanajras@Juans-MacBook-Pro.local>
2023-09-05 22:18:06 -0300

    Add HTML reporting capability

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  video: false,
+  reporterOptions: {
+  charts: true,
+  reportPageTitle: 'Cypress Inline Reporter',
+  embeddedScreenshots: true,
+  inlineAssets: true
+  },
   e2e: {
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: 'https://www.saucedemo.com'
   },
